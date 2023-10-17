0050a9fd01a0521b939f4ec8bf0bf8dfe3d4fbe4
Fabrizio D'Isidoro <fabrizioandres98@gmail.com>
2023-10-17 19:58:39 -0300

    feat(js): suma y resta de total funcionando, reinicio de checkboxes al seleccionar tamaño funcionando, falta formulario de cliente y array u objeto que contenga la info de la compra

--- script.js
+++ script.js
@@ -3,17 +3,21 @@ const burgers = document.querySelectorAll(".burgers button")
 const armadoAgregados = document.querySelectorAll('.armado__agregados input[type="checkbox"]')
 const armadoAderezos = document.querySelectorAll('.armado__aderezos input[type="checkbox"]')
 const armadoBebidas = document.querySelectorAll('.armado__bebida input[type="checkbox"]')
+const checkboxes = document.querySelectorAll('input[type="checkbox"]')
 const total = document.querySelector("#total")
 
 //PRECIO TOTAL
 let precioTotal = 0
-total.innerHTML = "TOTAL : $ " + PRECIO
 
 //TAMAÑO SELECCIONADO
 let tamañoSeleccionado = ""
 
 burgers.forEach(burger => {
     burger.addEventListener("click", function () {
+        checkboxes.forEach(checkbox => {
+            checkbox.checked = false
+        })
+
         tamañoSeleccionado = burger.name
         precioTotal = parseInt(burger.value)
         console.log(tamañoSeleccionado + precioTotal)
@@ -29,6 +33,7 @@ armadoAgregados.forEach(agregado => {
             if (!elementosAgregados.includes(agregado.name)) {
                 elementosAgregados.push(agregado.name)
                 precioTotal += parseInt(agregado.value)
+                total.innerHTML = "TOTAL : $ " + precioTotal
                 console.log(elementosAgregados)
                 console.log(precioTotal)
             }
@@ -36,6 +41,8 @@ armadoAgregados.forEach(agregado => {
             const index = elementosAgregados.indexOf(agregado.name)
             if (index !== -1) {
                 elementosAgregados.splice(index, 1)
+                precioTotal -= parseInt(agregado.value)
+                total.innerHTML = "TOTAL : $ " + precioTotal
                 console.log(elementosAgregados)
             }
         }
@@ -49,6 +56,7 @@ armadoAderezos.forEach(aderezo => {
     aderezo.addEventListener("change", function () {
         if (!elementosAderezos.includes(aderezo.name)) {
             elementosAderezos.push(aderezo.name)
+            total.innerHTML = "TOTAL : $ " + precioTotal
             console.log(elementosAderezos)
         } else {
             const index = elementosAderezos.indexOf(aderezo.name)
@@ -69,6 +77,7 @@ armadoBebidas.forEach(bebida => {
             if (!elementosBebidas.includes(bebida.name)) {
                 elementosBebidas.push(bebida.name)
                 precioTotal += parseInt(bebida.value)
+                total.innerHTML = "TOTAL : $ " + precioTotal
                 console.log(elementosBebidas)
                 console.log(precioTotal)
             }
@@ -76,6 +85,8 @@ armadoBebidas.forEach(bebida => {
             const index = elementosBebidas.indexOf(bebida.name)
             if (index !== -1) {
                 elementosBebidas.splice(index, 1)
+                precioTotal -= parseInt(bebida.value)
+                total.innerHTML = "TOTAL : $ " + precioTotal
                 console.log(elementosBebidas)
             }
         }
