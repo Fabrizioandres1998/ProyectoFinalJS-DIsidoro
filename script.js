4092ae2783647d2a379e8e2aa33c9420e78e4c26
Fabrizio D'Isidoro <fabrizioandres98@gmail.com>
2023-10-17 16:19:05 -0300

    feat(js): usando dom en el TOTAL para que se refleje el precio total en el html

--- script.js
+++ script.js
@@ -3,16 +3,20 @@ const burgers = document.querySelectorAll(".burgers button")
 const armadoAgregados = document.querySelectorAll('.armado__agregados input[type="checkbox"]')
 const armadoAderezos = document.querySelectorAll('.armado__aderezos input[type="checkbox"]')
 const armadoBebidas = document.querySelectorAll('.armado__bebida input[type="checkbox"]')
+const total = document.querySelector("#total")
+
 //PRECIO TOTAL
 let precioTotal = 0
+total.innerHTML = "TOTAL : $ " + PRECIO
+
 //TAMAÑO SELECCIONADO
 let tamañoSeleccionado = ""
 
 burgers.forEach(burger => {
     burger.addEventListener("click", function () {
-            tamañoSeleccionado = burger.name 
-            precioTotal = parseInt(burger.value)
-            console.log(tamañoSeleccionado + precioTotal)
+        tamañoSeleccionado = burger.name
+        precioTotal = parseInt(burger.value)
+        console.log(tamañoSeleccionado + precioTotal)
     })
 })
 
@@ -37,3 +41,44 @@ armadoAgregados.forEach(agregado => {
         }
     })
 })
+
+//ARMADO ADEREZOS
+let elementosAderezos = []
+
+armadoAderezos.forEach(aderezo => {
+    aderezo.addEventListener("change", function () {
+        if (!elementosAderezos.includes(aderezo.name)) {
+            elementosAderezos.push(aderezo.name)
+            console.log(elementosAderezos)
+        } else {
+            const index = elementosAderezos.indexOf(aderezo.name)
+            if (index !== -1) {
+                elementosAderezos.splice(index, 1)
+                console.log(elementosAderezos)
+            }
+        }
+    })
+})
+
+//ARMADO BEBIDA
+let elementosBebidas = []
+
+armadoBebidas.forEach(bebida => {
+    bebida.addEventListener("change", function () {
+        if (bebida.checked) {
+            if (!elementosBebidas.includes(bebida.name)) {
+                elementosBebidas.push(bebida.name)
+                precioTotal += parseInt(bebida.value)
+                console.log(elementosBebidas)
+                console.log(precioTotal)
+            }
+        } else {
+            const index = elementosBebidas.indexOf(bebida.name)
+            if (index !== -1) {
+                elementosBebidas.splice(index, 1)
+                console.log(elementosBebidas)
+            }
+        }
+    })
+})
+
