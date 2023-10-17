22c4526ad0f967e93949dec8b0351eea2dcb4564
Fabrizio D'Isidoro <fabrizioandres98@gmail.com>
2023-10-17 12:56:29 -0300

    feat(JS): optimizacion de codigo usando atributos value y name

--- script.js
+++ script.js
@@ -1,71 +1,35 @@
 //GET
-const burgers = document.querySelectorAll('.burgers .burgers__tamaño')
+const burgers = document.querySelectorAll(".burgers button")
 const armadoAgregados = document.querySelectorAll('.armado__agregados input[type="checkbox"]')
 const armadoAderezos = document.querySelectorAll('.armado__aderezos input[type="checkbox"]')
 const armadoBebidas = document.querySelectorAll('.armado__bebida input[type="checkbox"]')
 //PRECIO TOTAL
 let precioTotal = 0
 //TAMAÑO SELECCIONADO
-let tamañoSeleccionado = 0
+let tamañoSeleccionado = ""
 
 burgers.forEach(burger => {
     burger.addEventListener("click", function () {
-        tamañoSeleccionado = burger.id
-        console.log(tamañoSeleccionado)
-        precioBurger()
+            tamañoSeleccionado = burger.name 
+            precioTotal = parseInt(burger.value)
+            console.log(tamañoSeleccionado + precioTotal)
     })
 })
 
-function precioBurger() {
-    if (tamañoSeleccionado === "burgerS") {
-        precioTotal += 1500
-    }
-    if (tamañoSeleccionado === "burgerM") {
-        precioTotal += 1800
-    }
-    if (tamañoSeleccionado === "burgerL") {
-        precioTotal += 2100
-    }
-    console.log(precioTotal)
-}
 //ARMADO AGREGADOS
-let elementos = [{
-    nombre: "tomate",
-    precio: 200
-},
-{
-    nombre: "lechuga",
-    precio: 200
-},
-{
-    nombre: "bacon",
-    precio: 250
-},
-{
-    nombre: "huevo",
-    precio: 250
-},
-{
-    nombre: "cebolla",
-    precio: 200
-},
-{
-    nombre: "cheddar",
-    precio: 250
-}
-]
-
 let elementosAgregados = []
 
 armadoAgregados.forEach(agregado => {
     agregado.addEventListener("change", function () {
         if (agregado.checked) {
-            if (!elementosAgregados.includes(agregado.id)) {
-                elementosAgregados.push(agregado.id)
+            if (!elementosAgregados.includes(agregado.name)) {
+                elementosAgregados.push(agregado.name)
+                precioTotal += parseInt(agregado.value)
                 console.log(elementosAgregados)
+                console.log(precioTotal)
             }
         } else {
-            const index = elementosAgregados.indexOf(agregado.id)
+            const index = elementosAgregados.indexOf(agregado.name)
             if (index !== -1) {
                 elementosAgregados.splice(index, 1)
                 console.log(elementosAgregados)
@@ -73,4 +37,3 @@ armadoAgregados.forEach(agregado => {
         }
     })
 })
-
